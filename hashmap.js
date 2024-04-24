a34849176c4f3e7e67e36da13811a0702221417c
frarosset <37275581+frarosset@users.noreply.github.com>
2024-04-24 12:05:35 +0200

    Add #hashByDivision and #hashByMultiplication methods to HashMap
    These private methods will be used by the hash(key) method to take
    into consideration the size of the buckets array.
    
    They take the pure hash code computed converting the key to a number
    and modify it in order to get a valid index in the buckets array,
    returning an updated hash code.
    
    The simpler #hashByDivision should use a prime buckets size, while
    the size can be any number using #hashByMultiplication.
    see https://www.geeksforgeeks.org/what-are-hash-functions-and-how-to-choose-a-good-hash-function/
    
    Since the size of the buckets array is set as a multiple of 2, as
    it is usually done, the #hashByMultiplication is actually used.
    
    Also, to avoid numerical errors while iteratevly converting the
    string to a number (the number might indeed quickly become very
    large), we call #hashByMultiplication at each step of the process.

--- hashmap.js
+++ hashmap.js
@@ -7,6 +7,8 @@ class HashMap{
     #capacity;            // number of items in the hash table
     #buckets;             // array with buckets
 
+    #c = (Math.sqrt(5)-1)/2; // precomputed constant to be used by #hashByMultiplication() method
+
     constructor(){
         this.#size = this.#initial_size;
         this.#capacity = 0;
@@ -19,9 +21,27 @@ class HashMap{
         const prime = 31;
         for (let i=0; i<key.length; i++){
             hashCode = prime * hashCode + key.charCodeAt(i);
+            // Avoid numbers larger than the size of the buckets array.
+            // Since the size of the buckets array is set as a multiple of 2, as 
+            // it is usually done, the #hashByMultiplication is actually used.
+            // Also, to avoid numerical errors while iteratevly converting the 
+            // string to a number (the number might indeed quickly become very
+            // large), call #hashByMultiplication at each step of the process.
+            hashCode = this.#hashByMultiplication(hashCode);
         }
         return hashCode;
     }
+
+    // The simpler #hashByDivision should use a prime buckets size
+    // The size can be any number using #hashByMultiplication
+    // see https://www.geeksforgeeks.org/what-are-hash-functions-and-how-to-choose-a-good-hash-function/
+    #hashByDivision(hashCode){
+        return hashCode % this.#size;
+    }
+    #hashByMultiplication(hashCode){
+        return Math.floor(this.#size * ((hashCode * this.#c) % 1));
+    }
+
 }
 
 let hashMap = new HashMap();
